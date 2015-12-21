e99c83cd39c3f88f692a1b6b6caf8a0f7639ab13
Rain Ramm <rammrain@gmail.com>
2015-12-21 17:49:05 +0200

    Add autoloader config.

--- src/BitWeb/MailModule/Module.php
+++ src/BitWeb/MailModule/Module.php
@@ -18,4 +18,17 @@ class Module
     {
         return include __DIR__ . '/../../../config/module.config.php';
     }
+
+    public function getAutoloaderConfig()
+    {
+        $dir = dirname(dirname(dirname(__DIR__)));
+
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => $dir . '/src/' . __NAMESPACE__,
+                ],
+            ],
+        ];
+    }
 }
